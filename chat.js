ce82e439fe22e15ef0beae5bfbe2eb29497a452c
Martoblue <luismarto8a@gmail.com>
2024-02-02 16:53:20 -0300

    Terminado live chat con socket.io html js

--- chat.js
+++ chat.js
@@ -6,20 +6,41 @@ const newMessageTextArea = document.querySelector('#new-message');
 const sendButton = document.querySelector('#send-button');
 const userNameInput = document.querySelector('#user-name');
 
+const roomNameInput = document.querySelector('#room-name');
+const joinRoomButton = document.querySelector('#join-room-button');
+const joinGeneralButton = document.querySelector('#join-general-button');
+
+let currentRoom = 'general'; // Variable para almacenar la sala actual
+
+// Unirse a la sala general al cargar
+socket.emit('join general');
+
+joinRoomButton.addEventListener('click', () => {
+  currentRoom = roomNameInput.value;
+  socket.emit('join room', currentRoom);
+  messageContainer.innerHTML = ''; // Limpiar el contenedor de mensajes
+});
+
+joinGeneralButton.addEventListener('click', () => {
+  currentRoom = 'general';
+  socket.emit('join general');
+  messageContainer.innerHTML = ''; // Limpiar el contenedor de mensajes
+});
+
 
 sendButton.addEventListener('click', () => {
-    const message = newMessageTextArea.value;
-    const userName = userNameInput.value || 'Anónimo';
+  const message = newMessageTextArea.value;
+  const userName = userNameInput.value || 'Anónimo';
 
-    socket.emit('chat message', { userName, message });
+  socket.emit('chat message', { room: currentRoom, userName, message });
 
-    // Limpiar el campo de texto para el prox message
-    newMessageTextArea = '';
+  // Limpiar el campo de texto para el prox message
+  newMessageTextArea.value = '';
 });
 
 socket.on('chat message', (data) => {
-    const messageElement = document.createElement('div');
-    console.log(data);
-    messageElement.innerText = `${data.userName} dice: ${data.message}`;
-    messageContainer.appendChild(messageElement);
+  const messageElement = document.createElement('div');
+  // <div></div>
+  messageElement.innerText = `${data.userName} tell: ${data.message}`;
+  messageContainer.appendChild(messageElement);
 })
